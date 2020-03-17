2c55826f13da727a0566a20eb9526084eaee7940
Harish Bommakanti <33380236+harishbommakanti@users.noreply.github.com>
2020-03-17 11:45:39 -0500

    wrote rotate90degCW method to modify the resulting ASCII text

--- src/App.java
+++ src/App.java
@@ -7,18 +7,23 @@ import java.util.Scanner;
 
 public class App
 {
+    private static int width;
+    private static int height;
     public static void main(String[] args)
     {
         BufferedImage img = init();
+        width = img.getWidth();
+        height = img.getHeight();
+        //System.out.println(width + " " + height);
 
         //generates a 3d matrix with a rgb array at each row/col
-        int[][][] rgbTupleMatrix = generate2dMatrix(img);
+        String[][] rgbMatrix = generateRGBMatrix(img);
         //testing the rgbTupleMatrix
-        /*for (int[][] twoDmatrix : rgbTupleMatrix)
-            for (int[] oneDmatrix : twoDmatrix)
-                System.out.println(Arrays.toString(oneDmatrix)); */
+        /*for (String[] arr : rgbMatrix)
+            for (String str : arr)
+                System.out.println(str);*/
 
-        int[][] brightnessMatrix = generateBrightness(rgbTupleMatrix);
+        int[][] brightnessMatrix = generateBrightness(rgbMatrix);
         //testing the generateBrightness method
         /*for (int[] arr : brightnessMatrix)
             for (int brightness : arr)
@@ -30,22 +35,13 @@ public class App
             for (char j:i)
                 System.out.println(j);*/
 
-        //now, print out results
-        PrintWriter pw = null;
-        try{
-            pw = new PrintWriter(new FileWriter(new File("output.txt")));
-        } catch (IOException e){}
+        //printOut("images/beforeRotate.txt",characterMatrix);
 
-        for (int i = 0; i < characterMatrix.length; i++)
-        {
-            for (int j = 0; j < characterMatrix[0].length; j++)
-            {
-                pw.print(characterMatrix[i][j]);
-            }
-            pw.println();
-        }
-        pw.close();
-        System.out.println("Image to ASCII successfull! check for a 'output.txt' file to see the results.");
+        //now, for some reason, need to flip 90 degrees clockwise and rotate horizontally
+        characterMatrix = rotate90degCW(characterMatrix);
+
+        //now, print out results
+        printOut("images/afterRotate.txt", characterMatrix);
     }
 
     private static BufferedImage init()
@@ -72,35 +68,33 @@ public class App
         return image;
     }
 
-    private static int[][][] generate2dMatrix(BufferedImage img)
+    private static String[][] generateRGBMatrix(BufferedImage img)
     {
-        int width = img.getWidth();
-        int height = img.getHeight();
-        int[][][] rgbMatrix = new int[width][height][3];
-
-        for (int row = 0; row < width; row++)
+        String[][] rgbMatrix = new String[width][height];
+        for (int h = 0; h < height; h++)
         {
-            for (int col=0; col<height; col++)
+            for (int w = 0; w < width; w++)
             {
-                Color c = new Color(img.getRGB(row,col));
-                int[] pixelRGBMatrix = {c.getRed(),c.getGreen(),c.getBlue()};
-                rgbMatrix[row][col] = pixelRGBMatrix;
+                Color c = new Color(img.getRGB(w,h));
+                String colorString = c.getRed()+" "+c.getGreen()+" "+c.getBlue();
+                rgbMatrix[w][h] = colorString;
             }
         }
 
         return rgbMatrix;
     }
 
-    private static int[][] generateBrightness(int[][][] mat)
+    private static int[][] generateBrightness(String[][] mat)
     {
-        int[][] result = new int[mat.length][mat[0].length]; //assume its a rectangular image
+        int[][] result = new int[width][height]; //assume its a rectangular image
 
         for (int i = 0; i < mat.length; i++)
         {
             for (int j = 0; j < mat[i].length; j++)
             {
-                int[] rgb = mat[i][j];
-                int average = (int)(.21*rgb[0]+.72*rgb[1]+.07*rgb[2]); //formula to determine the brightness of current pixel
+                String rgb = mat[i][j];
+                String[] rgbVals = rgb.split(" ");
+                int average = (int)(.21*Integer.parseInt(rgbVals[0])+.72*Integer.parseInt(rgbVals[1])+.07*Integer.parseInt(rgbVals[2])); //formula to determine the brightness of current pixel
                 result[i][j] = average;
             }
         }
@@ -109,7 +103,7 @@ public class App
 
     private static char[][] convertBrightnessToASCII(int[][] matrix)
     {
-        char[][] result = new char[matrix.length][matrix[0].length];
+        char[][] result = new char[width][height];
 
         String characterArray = "`^\\\",:;Il!i~+_-?][}{1)(|\\\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"; //grabbed from internet to simulate brightness
         for (int i=0;i<matrix.length;i++)
@@ -126,5 +120,39 @@ public class App
             }
         }
         return result;
+    }
+
+    private static char[][] rotate90degCW(char[][] matrix)
+    {
+        char[][] result = new char[height][width];
+
+        for (int w = 0; w < width; w++)
+        {
+            for (int h = 0; h < height; h++)
+            {
+                result[h][width-w-1]=matrix[w][h];
+            }
+        }
+
+        return result;
+    }
+
+    private static void printOut(String filename, char[][] characterMatrix)
+    {
+        PrintWriter pw = null;
+        try{
+            pw = new PrintWriter(new FileWriter(new File(filename)));
+        } catch (IOException e){}
+
+        for (int i = 0; i < characterMatrix.length; i++)
+        {
+            for (int j = 0; j < characterMatrix[0].length; j++)
+            {
+                pw.print(characterMatrix[i][j]);
+            }
+            pw.println();
+        }
+        pw.close();
+        System.out.printf("Image to ASCII successfull! check for a %s file to see the results.\n",filename);
     }
 }
